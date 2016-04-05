3ffaa549407ac869a62dc2d21c534f316ad3f7eb
nikitaband1t <goldofcoin@yandex.ru>
2016-04-06 02:57:43 +0300

    Commit all pages

--- DoubleLinkedList.h
+++ DoubleLinkedList.h
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <memory>
 
-
 template<class T>
 struct Node {
     T data;
@@ -48,10 +47,38 @@ T Node<T>::remove() {
     return data;
 }
 
+template<class T>
+class MyIterator : std::iterator<std::input_iterator_tag, std::shared_ptr<Node<T>>> {
+    std::shared_ptr<Node<T>> ptr;
+public:
+    MyIterator(std::shared_ptr<Node<T>> val) {
+        ptr = val;
+    }
+
+    MyIterator(const MyIterator &mit) : MyIterator(mit.ptr) { }
+
+    MyIterator &operator++() {
+        ptr = ptr.get()->right;
+        return *this;
+    }
+
+    MyIterator &operator--() {
+        if (ptr->left != nullptr) {
+           ptr = ptr.get()->left;
+            return *this;
+        }
+    }
+
+    bool operator!=(const MyIterator &myIterator) { return ptr != myIterator.ptr; }
+
+    Node<T> &operator*() { return *ptr.get(); }
+
+};
 
 template<class T>
 class DoubleLinkedList {
 private:
+    int _length;
     std::shared_ptr<Node<T>> _begin, _end, _now;
 public:
     DoubleLinkedList();
@@ -62,33 +89,49 @@ public:
 
     T add_at(int ind);
 
-    T delete_by_index(int num);
+    T delete_by_index(long num);
 
-    T delete_first_elem(T elem);
+    T *delete_first_elem(T elem);
 
     std::shared_ptr<Node<T>> previous();
 
     std::shared_ptr<Node<T>> next();
 
-    //MyIterator &begin();
+    MyIterator<T> &begin();
 
-    //MyIterator &end();
+    MyIterator<T> &end();
 
 
     T remove();
 
-    T remove_if(bool f(T data));
+    DoubleLinkedList &remove_if(bool f(T data));
 
     T element_at(int ind);
 
+    long length();
 };
 
 template<class T>
 DoubleLinkedList<T>::DoubleLinkedList() {
+    _length = 0;
+}
+
+template<class T>
+
+MyIterator<T> &DoubleLinkedList<T>::begin() {
+    MyIterator<T> *bgt = new MyIterator<T>(_begin);
+    return *bgt;
+}
+
+template<class T>
+MyIterator<T> &DoubleLinkedList<T>::end() {
+    MyIterator<T> *bgt = new MyIterator<T>(_end);
+    return *bgt;
 }
 
 template<class T>
 T DoubleLinkedList<T>::push_back(T elem) {
+    _length++;
     if (_begin == nullptr) {
         _begin = std::shared_ptr<Node<T>>(new Node<T>(elem));
         _begin.get()->left = nullptr;
@@ -101,7 +144,6 @@ T DoubleLinkedList<T>::push_back(T elem) {
     return elem;
 }
 
-
 template<class T>
 T DoubleLinkedList<T>::element_at(int ind) {
     int itterator = 0;
@@ -111,6 +153,69 @@ T DoubleLinkedList<T>::element_at(int ind) {
         itterator++;
     }
     return begin_itt.get()->data;
+}
+
+
+template<class T>
+T DoubleLinkedList<T>::delete_by_index(long num) {
+    if (num >= 0 && num < _length) {
+        _length--;
+        if (num == 0) {
+            _begin = _begin.get()->right;
+        }
+        else if (num == (_length)) {
+            _end = _end.get()->left;
+        }
+        else {
+            int itterator = 0;
+            std::shared_ptr<Node<T>> begin_itt = _begin;
+            while (itterator != num) {
+                begin_itt = begin_itt.get()->right;
+                itterator++;
+            }
+            std::cout << begin_itt.use_count() << std::endl;
+            begin_itt.get()->right.get()->left = begin_itt.get()->left;
+            begin_itt.get()->left.get()->right = begin_itt.get()->right;
+            return begin_itt.get()->get_data();
+        }
+
+    }
+
+}
+
+template<class T>
+T DoubleLinkedList<T>::push_forward(T elem) {
+    _length++;
+    if (_begin == nullptr) {
+        _begin = std::shared_ptr<Node<T>>(new Node<T>(elem));
+        _begin.get()->left = nullptr;
+        _end = _begin;
+        return elem;
+    }
+    std::shared_ptr<Node<T>> new_begin(new Node<T>(elem));
+    new_begin.get()->right = _begin;
+    _begin.get()->left = new_begin;
+    _begin = new_begin;
+}
+
+template<class T>
+long DoubleLinkedList<T>::length() {
+    return _length;
+}
+
+
+template<class T>
+DoubleLinkedList &DoubleLinkedList<T>::remove_if(bool (*func)(T data)) {
+    std::shared_ptr<DoubleLinkedList> list_out(new DoubleLinkedList());
+
+    for (auto itt = begin(); itt != end(); ++itt) {
+        if (func((*itt).data)) {
+            list_out.get()->push_back((*itt).remove());
+            --itt;
+        }
+
+    }
+    return  *list_out.get();
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include "DoubleLinkedList.h"
 
+bool func(int x){
+    if(x>6)
+        return false;
+    return true;
+}
 
 int main() {
     DoubleLinkedList<int> doubleLinkedList;
     doubleLinkedList.push_back(5);
     doubleLinkedList.push_back(6);
-    std::cout<<doubleLinkedList.element_at(1)<<std::endl;
-    // doubleLinkedList.push_forward(7);
-    //for(auto itter_liked_list = doubleLinkedList.begin();itter_liked_list!=doubleLinkedList.end();++itter_liked_list){
-      //  std::cout<<(*itter_liked_list).data<<std::endl;
-    //}
+    doubleLinkedList.push_back(7);
+    doubleLinkedList.push_back(8);
+    doubleLinkedList.delete_by_index(3);
+    std::cout<<doubleLinkedList.element_at(2)<<std::endl;
+    std::cout<<doubleLinkedList.length()<<std::endl;
+
+
+    DoubleLinkedList<int> list;
+    list.push_forward(10);
+    list.push_back(6);
+    list.push_back(8);
+    list.push_forward(7);
+    std::cout<<list.element_at(0)<<std::endl;
+
+
+    for(auto itter_liked_list = doubleLinkedList.begin();itter_liked_list!=doubleLinkedList.end();++itter_liked_list){
+        std::cout<<(*itter_liked_list).data<<std::endl;
+    }
+    auto deleted = doubleLinkedList.remove_if(func);
+
+
     return 0;
 }
